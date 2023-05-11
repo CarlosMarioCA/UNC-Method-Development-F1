a7c7af71d44b246288ec729c900f7d259ca72bd5
carlosmarioca <camchavezag@unal.edu.co>
2023-05-11 14:08:45 -0500

    Update 3

--- trashcode.py
+++ trashcode.py
@@ -113,3 +113,42 @@ class Start(tk.Frame):
 
         for(key, values) in config.MODES.items():
             tk.Radiobutton(optionsFrame, text = "Und").pack()
+
+
+
+
+import tkinter as tk
+from data import style
+from screen import *
+from PIL import ImageTk, Image
+import os
+
+class Manager(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,**kwargs)
+        self.title(" - Formula1 Administrator - ")
+        self.geometry(style.DIMENSIONS)
+        self.resizable(False,False)
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.configure(background=style.BACKGROUND)
+        container.grid_columnconfigure(0,weight=1) #This can define how many columns are on the screen.
+        container.grid_rowconfigure(0,weight=1)
+        self.frames= {} #Dictionary that contains all the frames that will be shown in the screen at some time.
+
+        for F in (Screen):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+        self.show_frame(Start)
+
+    def show_frame(self, container):
+        frame = self.frames[container]
+        frame.tkraise() # Use this to put the frame in front, frame shown at this point.
+"""
+        for F in (Screen):
+            frame = F(container, self)
+            self.frames[F] = frame
+        self.show_frame(Start)
+"""
