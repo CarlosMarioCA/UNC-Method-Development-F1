4cc3692817cb7c1972452fd4b987b2838ddb3075
carlosmarioca <camchavezag@unal.edu.co>
2023-05-14 01:28:15 -0500

    Update 10

--- data/info.py
+++ data/info.py
@@ -1,2 +1,20 @@
 TABS = ["Start", "Teams", "Pilots", "Classification", "Carrers", "History", "Live"]
 TEAMS = ["RedBull", "Mercedes", "Ferrari", "AlphaTauri", "AlphaRomeo","Aston Martin", "Williams","McLaren","Alpine","Haas"]
+PILOTS = {"Max Verstapen":"RedBull","Lewis Hamilton":"Mercedes", "Sergio Perez":"RedBull", "George Rusell":"Mercedes",
+        "Charles Leclerc":"Ferrari","Carlos Sainz":"Ferrari","Fernando Alonso":"AstonMartin","EstebanOcon":"Alpine",
+        "Lance Stroll":"AstonMartin","Yuki Tsunoda":"AlphaTauri","Lucas Piastri":"McLaren","Alexander Albon":"Williams",
+        "Guanyu Zhou":"AlphaRomeo","Valteri Botas":"AlphaRomeo","Pierre Gasly":"Alpine","Lando Norris":"McLaren",
+        "Kevin Magnusen":"Hass","Nyck DeVries":"AlphaTauri","Logan Sargeant":"Williams","Nico Hulkenberg":"Hass"}
+
+POINTS = {"Max Verstapen":0,"Lewis Hamilton":0, "Sergio Perez":0, "George Rusell":0,
+        "Charles Leclerc":0,"Carlos Sainz":0,"Fernando Alonso":0,"EstebanOcon":0,
+        "Lance Stroll":0,"Yuki Tsunoda":0,"Lucas Piastri":0,"Alexander Albon":0,
+        "Guanyu Zhou":0,"Valteri Botas":0,"Pierre Gasly":0,"Lando Norris":0,
+        "Kevin Magnusen":0,"Nyck DeVries":0,"Logan Sargeant":0,"Nico Hulkenberg":0}
+
+CARRERS = {"Gulf Air Bahrain GP","STC Saudi Arabian GP","Rolex Australian GP","Azerbaijan GP","Crypto.com Miami GP",
+        "GP Monaco","AWS Gran Premio de España","Pirelli GP du Canada","Grosser Preis von Osterreich",
+        "Aramco British GP","Qatar Airways Hungarian","MSC Cruises Belgian","Heineken duth GP","Pirelli Gran Premio D'Italia",
+        "Singapore Airlines GP","Lenovo Japanese GP","Qatar Airways GP","Lenovo United States GP",
+        "Gran Premio de la Ciudad de Mexico","Rolex Grande Premio de Sao Paulo","Heineken Silver Las Vegas GP",
+        "Etihad Airways Abu Dhabi GP"}
--- screen.py
+++ screen.py
@@ -39,8 +39,8 @@ class Teams(tk.Frame):
         
         #Left side of the screen.
         left = tk.Frame(frame)
-        left.configure(bg=style.PAGE)
-        tk.Label(left, text=" CRUD ", relief="flat").pack(side="top", fill="x", pady = 20)
+        left.configure(bg=style.TEAMS)
+        tk.Label(left, text=" Teams Menu ", relief="flat").pack(side="top", fill="x", pady = 20)
         tk.Button(left, text=" Create new team ", relief="flat", command=lambda: self.showMenu(bot,"create")).pack(side="top", pady = 10)
         tk.Button(left, text=" Update team ", relief="flat").pack(side="top", pady = 10)
         tk.Button(left, text=" Delete ", relief="flat").pack(side="top", pady = 10)
@@ -58,6 +58,8 @@ class Teams(tk.Frame):
         teams = sorted(info.TEAMS)
         tree.heading('#0', text='ID')
         tree.heading('Teams', text='Teams')
+        tree.column("#0", width=20)
+        tree.column("Teams", width=200)
         for a,b in zip(number, teams):
             tree.insert('', 'end', text=str(a), values=b)
         
@@ -71,6 +73,7 @@ class Pilots(tk.Frame):
         super().__init__(container)
         name = info.TABS[1]
         im = ""
+        self.container = container
         self.init_widget(container)
         self.pack(side="left",fill="both",expand=True)
 
@@ -81,31 +84,33 @@ class Pilots(tk.Frame):
         
         #Left side of the screen.
         left = tk.Frame(frame)
-        left.configure(bg=style.PAGE)
-        tk.Label(left, text=" CRUD ", relief="flat").pack(side="top", fill="x", pady = 20)
-        tk.Button(left, text=" Create new team ", relief="flat", command=lambda: self.showMenu(bot,"create")).pack(side="top", pady = 10)
-        tk.Button(left, text=" Update team ", relief="flat").pack(side="top", pady = 10)
+        left.configure(bg=style.PILOTS)
+        tk.Label(left, text=" Pilots Menu ", relief="flat").pack(side="top", fill="x", pady = 20)
+        tk.Button(left, text=" Create new Pilot ", relief="flat", command=lambda: self.showMenu(bot,"create")).pack(side="top", pady = 10)
+        tk.Button(left, text=" Update Pilot ", relief="flat").pack(side="top", pady = 10)
         tk.Button(left, text=" Delete ", relief="flat").pack(side="top", pady = 10)
         left.pack(side="left",fill="both",expand=True)
 
         #Right side of the screen.
         right = tk.Frame(frame)
         right.pack(side="right",fill="both",expand=True)
-        tree = ttk.Treeview(right, columns=('Teams'))
+        tree = ttk.Treeview(right, columns=("Teams"))
         scrollbar_vertical = ttk.Scrollbar(right, orient='vertical', command=tree.yview)
         scrollbar_vertical.pack(side='right', fill='y')
         tree.configure(yscrollcommand=scrollbar_vertical.set)
 
         # Agregar encabezados de columna
-        number = list(range(1,len(info.TEAMS)+1))
-        teams = sorted(info.TEAMS)
-        tree.heading('#0', text='ID')
-        tree.heading('Teams', text='Teams')
-        for a,b in zip(number, teams):
-            tree.insert('', 'end', text=str(a), values=b)
+        number = list(range(1,len(info.PILOTS)+1))
+        pilots = sorted(info.PILOTS.keys())
+        tree.heading('#0', text='Pilots')
+        tree.heading('Teams', text='Team')
+        tree.column("#0", width=150)
+        tree.column("Teams", width=150)
+        for a,b in zip(number, pilots):
+            tree.insert('', 'end', text = b, values = info.PILOTS[b])
         
         # Agregar elementos
-        tree.pack(side="right", fill="both", expand=True)
+        tree.pack(side="left", fill="both", expand=True)
         right.pack(side="right",fill="both",expand=True)
 
 class Classification(tk.Frame):
@@ -114,8 +119,8 @@ class Classification(tk.Frame):
         super().__init__(container)
         name = info.TABS[1]
         im = ""
+        self.container = container
         self.init_widget(container)
-        self.configure(bg="yellow")
         self.pack(side="left",fill="both",expand=True)
 
     def init_widget(self,container):
@@ -124,33 +129,39 @@ class Classification(tk.Frame):
         
         #Left side of the screen.
         right = tk.Frame(frame)
-        right.configure(bg=style.PAGE)
-        tk.Label(right, text=" CRUD ", relief="flat").pack(side="top", fill="x", pady = 20)
-        tk.Button(right, text=" Create new team ", relief="flat", command=lambda: self.showMenu(bot,"create")).pack(side="top", pady = 10)
-        tk.Button(right, text=" Update team ", relief="flat").pack(side="top", pady = 10)
-        tk.Button(right, text=" Delete ", relief="flat").pack(side="top", pady = 10)
+        right.configure(bg=style.CLASSIFICATION)
+        tk.Label(right, text=" Menu Classification ", relief="flat").pack(side="top", fill="x", pady = 20)
         right.pack(side="right",fill="both",expand=True)
 
         #Right side of the screen.
         left = tk.Frame(frame)
         left.pack(side="left",fill="both",expand=True)
-        tree = ttk.Treeview(left, columns=('Teams'))
+        tree = ttk.Treeview(left, columns=("Points","Pilots","Teams"))
         scrollbar_vertical = ttk.Scrollbar(left, orient='vertical', command=tree.yview)
         scrollbar_vertical.pack(side='left', fill='y')
         tree.configure(yscrollcommand=scrollbar_vertical.set)
 
         # Agregar encabezados de columna
-        number = list(range(1,len(info.TEAMS)+1))
-        teams = sorted(info.TEAMS)
-        tree.heading('#0', text='ID')
+        number = list(range(1,len(info.PILOTS)+1))
+        score = sorted(info.POINTS.items(), key=lambda x: x[1], reverse=True)
+        cont = 0
+        tree.heading('#0', text='Position')
+        tree.heading('Points', text='Points')
+        tree.heading('Pilots', text='Pilots')
         tree.heading('Teams', text='Teams')
-        for a,b in zip(number, teams):
-            tree.insert('', 'end', text=str(a), values=b)
+        tree.column("#0", width=20)
+        tree.column("Points", width=20)
+        tree.column("Pilots", width=100)
+        tree.column("Teams", width=100)
+
+        for a in score:
+            cont += 1
+            tree.insert('', 'end', text=cont, values=(a[1],a[0],info.PILOTS[a[0]]))
+            
         
         # Agregar elementos
         tree.pack(side="right", fill="both", expand=True)
         right.pack(side="right",fill="both",expand=True)
-
 
 class Carrers(tk.Frame):
 
@@ -158,17 +169,43 @@ class Carrers(tk.Frame):
         super().__init__(container)
         name = info.TABS[1]
         im = ""
+        self.container = container
         self.init_widget(container)
-        self.pack(side="bottom",fill="both",expand=True)
+        self.pack(side="left",fill="both",expand=True)
 
     def init_widget(self,container):
-        #Loading the image
-        imFrame = tk.Frame(container)
-        im = ImageTk.PhotoImage(Image.open("images/love.jpg").resize((400,350),resample=Image.BICUBIC))
-        self.im = im
-        lbim = tk.Label(imFrame, image = im).pack(side="left", fill="both", expand=True)
-        lbtext = tk.Label(imFrame, text=style.ABOUT_US_DESCRIPTION, wraplength=300,justify="center").pack(side="right",fill="both",expand=True)
-        imFrame.pack(side="top", fill="both",expand=True,padx=20,pady=10)
+        frame = tk.Frame(container)
+        frame.pack(side="top", fill="both",expand=True)
+        
+        #Left side of the screen.
+        right = tk.Frame(frame)
+        right.configure(bg=style.CARRERS)
+        tk.Label(right, text=" Menu Carrers ", relief="flat").pack(side="top", fill="x", pady = 20)
+        tk.Button(right, text=" Create new team ", relief="flat", command=lambda: self.showMenu(bot,"create")).pack(side="top", pady = 10)
+        tk.Button(right, text=" Update team ", relief="flat").pack(side="top", pady = 10)
+        tk.Button(right, text=" Delete ", relief="flat").pack(side="top", pady = 10)
+        right.pack(side="right",fill="both",expand=True)
+
+        #Right side of the screen.
+        left = tk.Frame(frame)
+        tree = ttk.Treeview(left)
+        scrollbar_vertical = ttk.Scrollbar(left, orient='vertical', command=tree.yview)
+        scrollbar_vertical.pack(side='left', fill='y')
+        tree.configure(yscrollcommand=scrollbar_vertical.set)
+        left.pack(side="left",fill="both",expand=True)
+
+        # Agregar encabezados de columna
+        number = list(range(1,len(info.CARRERS)+1))
+        circuits = info.CARRERS
+        tree.heading('#0', text='Circuit')
+        tree.column("#0", width=200)
+
+        for a,b in zip(number, circuits):
+            tree.insert('', 'end', text=b)
+        
+        # Agregar elementos
+        tree.pack(side="right", fill="both", expand=True)
+        right.pack(side="right",fill="both",expand=True)
 
 class History(tk.Frame):
 
@@ -176,17 +213,43 @@ class History(tk.Frame):
         super().__init__(container)
         name = info.TABS[1]
         im = ""
+        self.container = container
         self.init_widget(container)
-        self.pack(side="bottom",fill="both",expand=True)
+        self.pack(side="left",fill="both",expand=True)
 
     def init_widget(self,container):
-        #Loading the image
-        imFrame = tk.Frame(container)
-        im = ImageTk.PhotoImage(Image.open("images/love.jpg").resize((400,350),resample=Image.BICUBIC))
-        self.im = im
-        lbim = tk.Label(imFrame, image = im).pack(side="left", fill="both", expand=True)
-        lbtext = tk.Label(imFrame, text=style.ABOUT_US_DESCRIPTION, wraplength=300,justify="center").pack(side="right",fill="both",expand=True)
-        imFrame.pack(side="top", fill="both",expand=True,padx=20,pady=10)
+        frame = tk.Frame(container)
+        frame.pack(side="top", fill="both",expand=True)
+        
+        #Left side of the screen.
+        right = tk.Frame(frame)
+        right.configure(bg=style.CARRERS)
+        tk.Label(right, text=" Menu Carrers ", relief="flat").pack(side="top", fill="x", pady = 20)
+        tk.Button(right, text=" Create new team ", relief="flat", command=lambda: self.showMenu(bot,"create")).pack(side="top", pady = 10)
+        tk.Button(right, text=" Update team ", relief="flat").pack(side="top", pady = 10)
+        tk.Button(right, text=" Delete ", relief="flat").pack(side="top", pady = 10)
+        right.pack(side="right",fill="both",expand=True)
+
+        #Right side of the screen.
+        left = tk.Frame(frame)
+        tree = ttk.Treeview(left)
+        scrollbar_vertical = ttk.Scrollbar(left, orient='vertical', command=tree.yview)
+        scrollbar_vertical.pack(side='left', fill='y')
+        tree.configure(yscrollcommand=scrollbar_vertical.set)
+        left.pack(side="left",fill="both",expand=True)
+
+        # Agregar encabezados de columna
+        number = list(range(1,len(info.CARRERS)+1))
+        circuits = info.CARRERS
+        tree.heading('#0', text='Circuit')
+        tree.column("#0", width=200)
+
+        for a,b in zip(number, circuits):
+            tree.insert('', 'end', text=b)
+        
+        # Agregar elementos
+        tree.pack(side="right", fill="both", expand=True)
+        right.pack(side="right",fill="both",expand=True)
 
 class Live(tk.Frame):
 
